a2e7d7bee758ed762685d3a1d665125ba7ba2f2f
jonaprojects <merkava234@gmail.com>
2024-10-04 09:14:41 +0300

    Migrated to using the Image tag also in the talking sprite component to improve performance

--- components/sprite/TalkingSprite.jsx
+++ components/sprite/TalkingSprite.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TextBox, { SmallTextBox } from "../textbox/Textbox";
+import Image from "next/image";
 
 export default function TalkingSprite(props) {
   return (
@@ -7,7 +8,14 @@ export default function TalkingSprite(props) {
       className={`w-32 md:w-48 relative ${props.className}`}
       onClick={props?.onClick ?? (() => {})}
     >
-      <img src={props.src} alt={props.alt} className="object-contain" />
+      <Image
+        src={props.src}
+        alt={props.alt}
+        className="object-contain"
+        width={500}   // You should specify a width and height for better optimization
+        height={500}  // Tailor this to your image's actual dimensions
+        layout="responsive" // Keeps the responsiveness behavior like in your Tailwind class
+      />
       {props.show && (
         <div className="relative">
           <SmallTextBox className="absolute -top-2" content={props.message} />
